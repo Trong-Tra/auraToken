868835e09c2a0822044f73246db99c0415d29213
Minh Trong <traminhtrong2604@gmail.com>
2024-08-03 21:02:20 +0700

    test: declare test variable

---
+++ test/AuraToken.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13
+
+import {Test, console2, stdError } from "forge-std/Test.sol";
+import {auraToken} from "../src/AuraToken.sol";
+
+contract AuraTest is Test{
+    // Variables
+    AuraToken public auraToken;
+    address supplyOwner = makeAddr("Faucet")
+    address user1 = makeAddr("User1")
+    address user2 = makeAddr("User2")
+    error ERC20InvalidReceiver(address receiver);
+    
+}
