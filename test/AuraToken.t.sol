a1f7d6b5c254868714fa530e45e34b043e6e0b11
Minh Trong <traminhtrong2604@gmail.com>
2024-08-03 21:16:39 +0700

    test: basic view test for token

--- test/AuraToken.t.sol
+++ test/AuraToken.t.sol
@@ -1,24 +1,50 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13
+pragma solidity ^0.8.13;
 
 import {Test, console2, stdError } from "forge-std/Test.sol";
 import {auraToken} from "../src/AuraToken.sol";
 
 contract AuraTest is Test{
     // Variables
-    AuraToken public auraToken;
-    address supplyOwner = makeAddr("Faucet")
-    address user1 = makeAddr("User1")
-    address user2 = makeAddr("User2")
+    auraToken public token;
+    address supplyOwner = makeAddr("Faucet");
+    address user1 = makeAddr("User1");
+    address user2 = makeAddr("User2");
     error ERC20InvalidReceiver(address receiver);
 
     /**
-     * @dev setup test
+     * @dev setup contract
      */
-
     function setUp() public {
-        vm.prank(supplyOwner)
-        token = new auraToken("Aura token", "AURA", 100000)
+        vm.prank(supplyOwner);
+        token = new auraToken("Aura token", "AURA", 100000);
     }
 
+    /**
+     * @dev setup token name
+     */
+    function test_name() public {
+        assertEq(token.name(), "Aura token");
+    }
+
+    /**
+     * @dev setup token symbol
+     */
+    function test_symbol() public {
+        assertEq(token.symbol(), "AURA");
+    }
+
+    /**
+     * @dev setup token decimals
+     */
+    function test_decimals() public {
+        assertEq(token.decimals(), 18);
+    }
+
+    /**
+     * @dev setup token supply
+     */
+    function test_Supply() public {
+        assertEq(token.totalSupply(), 100000);
+    }
 }
