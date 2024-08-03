01561c446f0c8e5ccccb806ec816a920e093a5de
Minh Trong <traminhtrong2604@gmail.com>
2024-08-03 20:35:08 +0700

    build: token constructor

---
+++ src/AuraToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract auraToken is ERC20 {
+    /**
+     * @dev init constructor
+     *
+     */
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 mintedToken_
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, mintedToken_);
+    }
+}
